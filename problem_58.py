5d734d039584de687399f92665ab9fa413536478
mrajesh0328 <mrajesh0328@gmail.com>
2026-01-30 15:53:38 +0530

    Update problem_58.py

--- problem_58.py
+++ problem_58.py
@@ -1,6 +1,3 @@
-# Problem 58: Binary search implementation
-# Find and fix the error
-
 def binary_search(arr, target):
     left, right = 0, len(arr) - 1
     while left <= right:
@@ -8,9 +5,9 @@ def binary_search(arr, target):
         if arr[mid] == target:
             return mid
         elif arr[mid] < target:
-            left = mid
+            left = mid + 1
         else:
-            right = mid
+            right = mid - 1
     return -1
 
 numbers = [1, 3, 5, 7, 9, 11, 13, 15]
